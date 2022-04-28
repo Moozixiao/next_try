9a80d2dc77aa6d3c2e6efe4681d93dc52e7cd0e3
lixiaoxin <lixiaoxin@ly-sky.com>
2022-04-28 09:53:02 +0800

    fix: 修改deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 # deploy.sh
 # 错误时停止
 set -e
+# 1、下依赖
+npm install
 
-# 1、打包
-yarn build
+# 2、打包
+npm run build
 
 # 2、进入目标文件夹，这里跟项目打包输出的文件夹相同（如果项目中不修改默认为：dist）
 cd out
@@ -14,5 +16,5 @@ git add -A
 git commit -m 'deploy'
 
 # 4、将打包出的所有文件push到github远程仓库data-view的gh-pages 分支，如果远程没有这个分支也不要紧，会自动创建 git push -f https://github.com/Moozixiao/next_try.git master:gh-pages
-git push -f https://github.com/Moozixiao/next_try.git master
+git push -f https://github.com/Moozixiao/next_try.git master:gh-pages
 cd -
