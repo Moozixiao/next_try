0617ce84726f824a3ffbb57a3056c844b002f759
lixiaoxin <lixiaoxin@ly-sky.com>
2022-04-28 09:37:23 +0800

    fix: 修改login

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,31 +1,7 @@
-/*
- * @Description: 
- * @Author: lixiaoxin
- * @Date: 2022-04-13 10:03:11
- * @LastEditors: lixiaoxin
- * @LastEditTime: 2022-04-21 11:50:39
- */
-import React from 'react'
-import { useDrag } from 'react-dnd'
-import { ItemTypes } from './Constants'
+import React, { Component } from "react";
 
-/**
- * Your Component
- */
-export default function Card({ isDragging, text }) {
-  const [{ opacity }, dragRef] = useDrag(
-    () => ({
-      type: ItemTypes.CARD,
-      item: { text },
-      collect: (monitor) => ({
-        opacity: monitor.isDragging() ? 0.5 : 1
-      })
-    }),
-    []
-  )
-  return (
-    <div ref={dragRef} style={{ opacity }}>
-      {text}
-    </div>
-  )
+export default class Index extends Component {
+  render() {
+    return <div>Index</div>;
+  }
 }
